8d716bcefbb96d4e59074ddcf73351783781a72b
Aroob-Abdullah <aroob.alosaili1@gmail.com>
2026-05-03 22:42:23 +0300

    Update artist.h

--- artist.h
+++ artist.h
@@ -3,17 +3,17 @@
 #define ARTIST_H
 
 typedef union {
-    char phone[20];
-    char nationalId[20];
-    char address[100];
+   char phone[20];
+   char nationalId[20];
+   char address[100];
 } ArtistInfo;
 
- struct Artist {
-    int id;
-    char name[50];
-    int infoType;
-    ArtistInfo info;
-    struct Artist* next;
+struct Artist {
+   int id;
+   char name[50];
+   int infoType;
+   ArtistInfo info;
+   struct Artist* next;
 } ;
 
 void addArtist(struct Artist** head, int id, const char* name, int infoType, const char* info);
